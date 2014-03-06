14815f589658cc741681077608a331db90eedb99
DustyLenz <albertnov29@gmail.com>
2014-03-05 22:07:14 -0500

    Added Test Comment

--- FinalVersion/project.sql
+++ FinalVersion/project.sql
@@ -1,6 +1,6 @@
 .mode column
 .headers ON
-
+--Testing
 create table customer (cust_id integer, cust_name varchar(200), address varchar(250), phone bigint, email varchar(50), total_boxes int, total_price numeric(12,2), primary key(cust_id asc));
 
 create table c_order (order_id integer, cust_name varchar(200), scout_name varchar(200), type varchar(50), amount int, paid int, picked_up int, primary key(order_id asc), foreign key(cust_name) references customer(cust_name), foreign key(scout_name) references customer(scout_name), foreign key(type) references cookie(type));
